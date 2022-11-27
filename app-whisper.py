522d0290c032723e6881b3d1d32ec5843454208f
SebasUrbina <sebastian.urbina@live.cl>
2022-11-27 16:41:23 -0300

    add upload file code streamlit

--- app-whisper.py
+++ app-whisper.py
@@ -1,10 +1,27 @@
 import streamlit as st
+import numpy as np
+# import whisper
 
-#Contexto
+# Contexto
 st.title('Whisper APP')
 st.markdown('')
 
-file = st.file_uploader("Suba el archivo mp3 a transcribit")
+audio_file = st.file_uploader("Suba el archivo mp3 a transcribir")
 
-transcripcion = 'Esta es la transcripción de ejemplo v2'
-output_text = st.text_area('Transcripción', transcripcion, height=150)
+# Cargar audio
+
+if audio_file is not None:
+    st.audio(audio_file, format='audio/wav')
+
+
+# Cargar modelo
+# model = whisper.load_model("base")
+# transcripcion = model.transcribe('despacito.mp3')
+
+if st.button('Transcribir audio'):
+    st.write('Transcribiendo audio...')
+    transcripcion = 'Esta es la transcripción de ejemplo v2'
+    output_text = st.text_area('Transcripción', transcripcion, height=150)
+
+else:
+    st.write('Prueba')
