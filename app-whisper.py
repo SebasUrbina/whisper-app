af7921d07edf3242c282f5dfa7ad669bffd9c8d5
SebasUrbina <48739508+SebasUrbina@users.noreply.github.com>
2022-11-27 15:33:52 -0300

    Initial commit

---
+++ app-whisper.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#Contexto
+st.title('Whisper APP')
+st.markdown('')
+
+file = st.file_uploader("Suba el archivo mp3 a transcribit")
+
+transcripcion = 'Esta es la transcripción de ejemplo'
+output_text = st.text_area('Transcripción', transcripcion, height=150)
