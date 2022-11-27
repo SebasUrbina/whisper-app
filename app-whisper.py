b7af34d6f350a6370ca3303c8f1ddcc5a20c1435
SebasUrbina <sebastian.urbina@live.cl>
2022-11-27 16:01:15 -0300

    cambia texto transcripcion por defecto

--- app-whisper.py
+++ app-whisper.py
@@ -6,5 +6,5 @@ st.markdown('')
 
 file = st.file_uploader("Suba el archivo mp3 a transcribit")
 
-transcripcion = 'Esta es la transcripción de ejemplo'
+transcripcion = 'Esta es la transcripción de ejemplo v2'
 output_text = st.text_area('Transcripción', transcripcion, height=150)
