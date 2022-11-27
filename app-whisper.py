f3389615d1b70cbed46f8fb30cd8c0fcaef47d50
SebasUrbina <sebastian.urbina@live.cl>
2022-11-27 18:42:23 -0300

    update app -> hacer transcripción de archivo mp3 como input

--- app-whisper.py
+++ app-whisper.py
@@ -1,27 +1,31 @@
 import streamlit as st
 import numpy as np
-# import whisper
+import time
+import whisper
+import os
 
 # Contexto
 st.title('Whisper APP')
 st.markdown('')
 
+with st.spinner('Cargando modelo'):
+    model = whisper.load_model("tiny")
+
 audio_file = st.file_uploader("Suba el archivo mp3 a transcribir")
-
 # Cargar audio
-
 if audio_file is not None:
     st.audio(audio_file, format='audio/wav')
+    audio_name = audio_file.name
+    with open(audio_name, 'wb') as f:
+        f.write(audio_file.read())
 
-
-# Cargar modelo
-# model = whisper.load_model("base")
-# transcripcion = model.transcribe('despacito.mp3')
-
-if st.button('Transcribir audio'):
-    st.write('Transcribiendo audio...')
-    transcripcion = 'Esta es la transcripción de ejemplo v2'
-    output_text = st.text_area('Transcripción', transcripcion, height=150)
-
-else:
-    st.write('Prueba')
+    # Cargar modelo
+    if st.button('Transcribir audio'):
+        # st.warning('Transcribiendo audio...', icon="⚠️")
+        with st.spinner('Transcribiendo audio'):
+            output = model.transcribe(audio_name)
+            transcripcion = output['text']
+        st.success('Audio transcrito correctamente', icon="✅")
+        output_text = st.text_area(
+            'Transcripción', transcripcion, height=150, label_visibility='hidden')
+    os.remove(audio_name)
